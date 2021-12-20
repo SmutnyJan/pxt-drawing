a9106ca705808ced97480d2c3259dca539fb88d8
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2021-12-21 00:08:46 +0100

    hide cursor + edit based on feedback

--- main.ts
+++ main.ts
@@ -1,14 +1,36 @@
-input.onButtonPressed(Button.A, function () {
-    if (stateBeforeArrive) {
-        led.plot(x, y)
-    } else {
-        led.unplot(x, y)
-    }
-    x += 1
-    x = x % 5
-    stateBeforeArrive = led.point(x, y)
+input.onGesture(Gesture.TiltRight, function () {
+    pohyb(x + 1, y)
 })
-input.onButtonPressed(Button.AB, function () {
+input.onGesture(Gesture.TiltLeft, function () {
+    pohyb(x - 1, y)
+})
+input.onGesture(Gesture.LogoUp, function () {
+    pohyb(x, y + 1)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    pohyb(x, y - 1)
+})
+
+
+function pohyb(newX: number, newY: number) {
+    if (showCursor) {
+        if (stateBeforeArrive) {
+            led.plot(x, y)
+        } else {
+            led.unplot(x, y)
+        }
+        x = newX
+        y = newY
+        if (newX > 4) x = 0
+        else if (newX < 0) x = 4
+
+        if (newY > 4) y = 0
+        else if (newY < 0) y = 4
+
+        stateBeforeArrive = led.point(x, y)
+    }
+}
+input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
         . . . . .
         . . . . .
@@ -16,28 +38,34 @@ input.onButtonPressed(Button.AB, function () {
         . . . . .
         . . . . .
         `)
-    stateBeforeArrive = false    
+    stateBeforeArrive = false
 })
+
 input.onButtonPressed(Button.B, function () {
-    if (stateBeforeArrive) {
+
+    showCursor = !showCursor
+    if(stateBeforeArrive) {
         led.plot(x, y)
-    } else {
-        led.unplot(x, y)
     }
-    y += 1
-    y = y % 5
-    stateBeforeArrive = led.point(x, y)
+    else {
+        led.unplot(x,y)
+    }
+
 })
+
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     stateBeforeArrive = !(stateBeforeArrive)
 })
 let stateBeforeArrive = false
+let showCursor = true
 let y = 0
 let x = 0
 x = 2
 y = 2
 stateBeforeArrive = led.point(x, y)
 basic.forever(function () {
-    led.toggle(x, y)
-    basic.pause(100)
+    if (showCursor) {
+        led.toggle(x, y)
+        basic.pause(100)
+    }
 })
